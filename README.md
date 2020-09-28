775b0167c10af2dead012b0e09f998bf60435327
chrispearson <cpearson@apterainc.com>
2020-09-28 12:32:20 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+# Getting Started
+To Get Started, make sure you have the local.settings.json file created in the api directory
+You will also need to install Azure Functions extension for VS Code : https://marketplace.visualstudio.com/items?itemName=ms-azuretools.vscode-azurefunctions
+
 # Arduino
 Project API uses node.js: https://docs.microsoft.com/en-us/azure/azure-functions/functions-reference-node
+
--- api/GetTopItem/index.js
+++ api/GetTopItem/index.js
@@ -1,28 +1,28 @@
 var azure = require("azure-storage");
-const { v4: uuidv4 } = require('uuid');
-
 module.exports = async function (context, req) {
-
     var tableSvc = azure.createTableService('apteraarduino',process.env["AzureTableStorageAccessKey"]);
     var query = new azure.TableQuery()
         .top(1)
         .where('RowKey eq ?','000062ce-81c4-4c2d-9d7d-8a56be704c1d');
+    
+    tableSvc.queryEntities('cpdata', query, null, function(error, result, response) {
+    if (!error) {
+        // result.entries contains entities matching the query
         
-        tableSvc.queryEntities('cpdata', query, null, function(error, result, response) {
-        if (!error) {
-          // result.entries contains entities matching the query
-          context.res = {
+        context.res = {
+        // status: 200, /* Defaults to 200 */
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: result.entries[0].value._
+        };
+    } else {
+        context.res = {
             // status: 200, /* Defaults to 200 */
-            body: result.entries
+            body: "error"
             };
-        } else {
-            context.res = {
-                // status: 200, /* Defaults to 200 */
-                body: "error"
-                };
-        }
+    }
     });
     
-
     context.log('JavaScript HTTP trigger function processed a request.');
 }
