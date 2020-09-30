4d0d5b8de23d332a510cab8576758fa1aa6efa32
chrispearson <cpearson@apterainc.com>
2020-09-30 15:02:22 -0400

    adjusted query to pull latest

--- api/GetTopItem/index.js
+++ api/GetTopItem/index.js
@@ -7,9 +7,13 @@ module.exports = async function (context, req) {
 
 async function GetValue() {
     let tableSvc = azure.createTableService('apteraarduino',process.env["AzureTableStorageAccessKey"]);
+
+    let lstHr = new Date(Date.now.ear)
+
     let query = new azure.TableQuery()
         .top(1)
-        .where('RowKey eq ?','000062ce-81c4-4c2d-9d7d-8a56be704c1d');
+        .where('Timestamp >= ?date?', new Date(new Date().setSeconds(-10)));
+        
     let r = await queryEntities(tableSvc, 'cpdata', query, null);
     return r.entries[0].value._;
 }
