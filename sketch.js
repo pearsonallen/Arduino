c38ccda8a4b5f0d4d54e241ca05a552d01932e06
chrispearson <cpearson@apterainc.com>
2020-09-30 19:30:44 -0400

    update and refactor

--- sketch.js
+++ sketch.js
@@ -2,17 +2,20 @@ function setup() {
     createCanvas(800, 800);
     angleMode(DEGREES);
     x = width / 2;
-    y = 100;
+    y = 300;
 
     getValue();
   }
-  //banana position variables
-let y;
-let x;
-//banana color variables
-let r1 = 206;
-let g1 = 239;
-let b1 = 107;
+
+  let f = 4.25;
+
+  let y;
+  let x;
+  //banana color variables
+  let r1 = 206;
+  let g1 = 239;
+  let b1 = 107;
+
   function makeBanana(s) {
     background(0);
     drawText(s);
@@ -29,28 +32,28 @@ let b1 = 107;
   }
 
   function determineColor(s) {
-    //make the banana change color based on y height 
     if (s >= -0 && s < 500) {
-      r1 = 226;
-      g1 = 250;
-      b1 = 108;
+      r1 = 80;
+      g1 = 64;
+      b1 = 0;
       stroke(r1, g1, b1);
     }
     if (s >= 500 && s < 1000) {
-      r1 = 255;
-      g1 = 240;
-      b1 = 30;
-      stroke(r1, g1, b1);
-    }
-    if (s >= 1000 && s < 3000) {
       r1 = 232;
       g1 = 196;
       b1 = 0;
       stroke(r1, g1, b1);
-    } else if (s >= 3000) {
-      r1 = 80;
-      g1 = 64;
-      b1 = 0;
+    }
+    if (s >= 1000 && s < 3200) {
+      r1 = 255;
+      g1 = 240;
+      b1 = 30;
+      stroke(r1, g1, b1);
+    } else if (s >= 3200) {
+      r1 = 226;
+      g1 = 250;
+      b1 = 108;
+      stroke(r1, g1, b1);
     }
   }
 
@@ -58,9 +61,16 @@ let b1 = 107;
     noStroke();
     determineColor(s);
     fill(r1, g1, b1);
-    arc(x + random(3), y, 100, 100, 20, 200, OPEN);
+    arc(x, y, 
+      100 * f, 100 * f, 
+      20, 200, 
+      OPEN);
     fill(0);
-    arc(x, y - 25, 140, 100, 0, 180, OPEN);
+    stroke(0);
+    arc(x, y - (25 * f) , 
+      140 * f, 100 * f, 
+      0, 180, 
+      OPEN);
   }
 
   function doAction(response) {
